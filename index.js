aca10d3eef2a240573414e17aa37b37e8b4d1ce5
Mohammad <moh.hossain98@gmail.com>
2024-01-31 17:48:45 -0500

    added css

--- index.js
+++ index.js
@@ -23,3 +23,7 @@ console.log("Hello World");
 
 // ! Exercise
 // create three buttons with the text "Red", "Green", "Blue". When you click on the button, the background color of the page should change to the color of the button.
+
+// ! Exercise
+
+// use the form to create a to-do item. When you submit the form, the to-do item should be added as an 'li' to the ul with the id "todo-list". Each 'li' should have a delete button. When you click on the delete button, the 'li' should be removed from the list.
