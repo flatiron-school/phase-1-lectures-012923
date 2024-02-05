9d0c896b4119ecb4a3ae1ab8a36302513e377cae
Mohammad <moh.hossain98@gmail.com>
2024-02-05 13:06:39 -0500

    added codes

--- index.js
+++ index.js
@@ -1,59 +1,15 @@
-const students = [
-  {
-    name: "John Doe",
-    age: 20,
-    grade: 3.8,
-    subjects: ["Mathematics", "Physics", "English"],
-  },
-  {
-    name: "Jane Smith",
-    age: 19,
-    grade: 3.9,
-    subjects: ["Biology", "Chemistry", "History"],
-  },
-  {
-    name: "Michael Johnson",
-    age: 21,
-    grade: 3.5,
-    subjects: ["Computer Science", "Literature", "Art"],
-  },
-  {
-    name: "Emily Brown",
-    age: 18,
-    grade: 4.0,
-    subjects: ["Geography", "Spanish", "Economics"],
-  },
-  {
-    name: "David Wilson",
-    age: 22,
-    grade: 3.6,
-    subjects: ["Music", "Physical Education", "Psychology", "Reading"],
-  },
-  {
-    name: "Sophia Martinez",
-    age: 20,
-    grade: 3.7,
-    subjects: ["Sociology", "Statistics", "Drama"],
-  },
-  {
-    name: "Ethan Taylor",
-    age: 19,
-    grade: 3.9,
-    subjects: ["Anthropology", "Philosophy", "French"],
-  },
-  {
-    name: "Olivia Anderson",
-    age: 21,
-    grade: 3.8,
-    subjects: ["Environmental Science", "Political Science", "Astronomy"],
-  },
-  {
-    name: "William Thomas",
-    age: 20,
-    grade: 3.7,
-    subjects: ["Engineering", "Design", "Ethics"],
-  },
-];
+// fetch() - API
+console.log("Starting fetch");
+
+fetch("https://large-sunset-yttrium.glitch.me/students/")
+  .then((response) => response.json())
+  .then((students) => {
+    for (let student of students) {
+      renderStudentCard(student);
+    }
+  });
+
+console.log("End of fetch");
 
 // ! Student management system
 // * Display all students from the list
@@ -93,11 +49,6 @@ function renderStudentCard(student) {
   }
 }
 
-for (let student of students) {
-  //   console.log(student.subjects);
-  renderStudentCard(student);
-}
-
 const handleSubmit = (e) => {
   e.preventDefault();
 
@@ -116,4 +67,11 @@ form.addEventListener("submit", handleSubmit);
 // loop over the array
 // for each student create a student card
 
-console.log(students);
+//
+// <!-- <div class="advisor-card">
+//           <h2>Advisor Name</h2>
+//           <p>email@amsdmas</p>
+//           <p>2131241324</p>
+//         </div> --> append it to "advisor-container div"
+
+// GET : https://large-sunset-yttrium.glitch.me/advisors
